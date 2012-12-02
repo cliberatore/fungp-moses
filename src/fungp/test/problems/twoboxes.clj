4620bf0f1ef4400997f44b92f3fe81cc4b527d7c
cliberatore <chris.liberatore@gmail.com>
2012-12-02 03:20:06 -0800

    More testing

--- src/fungp/test/problems/twoboxes.clj
+++ src/fungp/test/problems/twoboxes.clj
@@ -7,22 +7,54 @@
   (:use clojure.pprint)
   (:use clojure.java.io))
 
-(def ^:dynamic lastTime)
-
 (defn test-moses-twobox [iters]
-  (with-open [wrtr (writer (str "reports/twobox." now ".time") :append true)]
+  (with-open [wrtr (writer (str "reports/twobox.moses." now ".time") :append true)]
     (loop [iters iters]
-      (let [runtime (time-body (rest (run-fungp-moses twobox-options)))]
+      (let [runtime (time-body (rest (run-fungp-moses twobox-moses-options)))]
          (.write wrtr (str runtime))
       (if (> iters 0)
         (recur (- iters 1))
         runtime)))))
 
-(defn sample-report-test-gp
+(defn test-fungp-twobox [iters]
+  (with-open [wrtr (writer (str "reports/twobox.fungp." now ".time") :append true)]
+    (loop [iters iters]
+      (let [runtime (time-body (rest (run-genetic-programming twobox-fungp-options)))]
+         (.write wrtr (str runtime))
+      (if (> iters 0)
+        (recur (- iters 1))
+        runtime)))))
+
+(defn report-twobox-moses
   "Reporting function. Prints out the tree and its score"
   [iteration best-tree best-fit]
 	  (pprint best-tree)
 	  (println (str "Iteration: " iteration "\tError:\t" best-fit "\n"))
 	  (flush)
-    (with-open [wrtr (writer (str "reports/twobox." now) :append true)]
-      (.write wrtr (str iteration "," best-fit ",(let " (nth best-tree 1) " " (str (nth best-tree 2))")\n"))))
+    (with-open [wrtr (writer (str "reports/twobox.moses." now) :append true)]
+      (.write wrtr (str iteration "," best-fit ",(let " (nth best-tree 1) " " (str (apply str (nth best-tree 2)))")\n"))))
+
+(defn report-twobox-fungp
+  "Reporting function. Prints out the tree and its score"
+  [iteration best-tree best-fit]
+	  (pprint best-tree)
+	  (println (str "Iteration: " iteration "\tError:\t" best-fit "\n"))
+	  (flush)
+    (with-open [wrtr (writer (str "reports/twobox.fungp." now) :append true)]
+      (.write wrtr (str iteration "," best-fit ",(let " (nth best-tree 1) " " (str (apply str (nth best-tree 2)))")\n"))))
+
+(def twobox-moses-options {:iterations 1 :migrations 51
+                  :terminals sample-parameters :max-depth 10
+                  :numbers number-literals :fitness sample-fitness
+                  :functions sample-functions :report report-twobox-moses
+                  :num-islands 6 :population-size 2000
+                  :adf-count 2
+                  :moses-normalize normalize-tree-regression})
+
+(def twobox-fungp-options {:iterations 1 :migrations 51
+                  :terminals sample-parameters :max-depth 10
+                  :numbers number-literals :fitness sample-fitness
+                  :functions sample-functions :report report-twobox-fungp
+                  :num-islands 6 :population-size 2000
+                  :adf-count 2
+                  :moses-normalize normalize-tree-regression})
