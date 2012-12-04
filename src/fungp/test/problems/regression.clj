0d2a7f577b255048c8c3f9e8790645308567b4f3
cliberatore <chris.liberatore@gmail.com>
2012-12-03 23:50:57 -0800

    Added some testing utilities

--- src/fungp/test/problems/regression.clj
+++ src/fungp/test/problems/regression.clj
@@ -24,15 +24,15 @@
 (def regression-options {:iterations 5 :migrations 10
                   :terminals sample-parameters :max-depth 10
                   :numbers number-literals :fitness sample-fitness
-                  :functions sample-functions :report report-regression-moses
+                  :functions sample-functions
                   :num-islands 6 :population-size 500
                   :adf-count 0 })
 
-(def regression-options-test {:iterations 1 :migrations 50
+(def regression-options-test {:iterations 1 :migrations 10
                   :terminals sample-parameters :max-depth 10
                   :numbers number-literals :fitness sample-fitness
-                  :functions sample-functions :report report-regression-moses
-                  :num-islands 6 :population-size 100
+                  :functions sample-functions
+                  :num-islands 1 :population-size 10
                   :adf-count 0
                   :moses-normalization normalize-tree-regression
                   :moses-optimization optimize-tree-regression})
@@ -43,46 +43,11 @@
 (def regression-opt-options (conj {:moses-optimization optimize-tree-regression} regression-options))
 
 (defn test-fungp [report-name samples options]
-  (loop [iters samples]
-    (let [runtime (time-body (rest (run-fungp-moses options)))]
-      (with-open [wrtr (writer (str "reports/" report-name now ".time") :append true)]
-        (.write wrtr (str (first runtime) "," (last (last runtime)) "\n")))
-      (if (> iters 0)
-        (recur (- iters 1))
-        runtime))))
-
-(defn test-both-regression [iters]
-  (loop [iters iters]
-    (let [runtime (time-body (rest (run-fungp-moses regression-both-options)))]
-      (with-open [wrtr (writer (str "reports/regression.both." now ".time") :append true)]
-        (.write wrtr (str (first runtime) "," (last (last runtime)) "\n")))
-      (if (> iters 0)
-        (recur (- iters 1))
-        runtime))))
-
-(defn test-opt-regression [iters]
-  (loop [iters iters]
-    (let [runtime (time-body (rest (run-fungp-moses regression-opt-options)))]
-      (with-open [wrtr (writer (str "reports/regression.opt." now ".time") :append true)]
-        (.write wrtr (str (first runtime) "," (last (last runtime)) "\n")))
-      (if (> iters 0)
-        (recur (- iters 1))
-        runtime))))
-
-(defn test-norm-regression [iters]
-    (loop [iters iters]
-      (let [runtime (time-body (rest (run-fungp-moses regression-norm-options)))]
-        (with-open [wrtr (writer (str "reports/regression.norm." now ".time") :append true)]
-         (.write wrtr (str (first runtime) "," (last (last runtime)) "\n")))
-      (if (> iters 0)
-        (recur (- iters 1))
-        runtime))))
-
-(defn test-fungp-regression [iters]
-    (loop [iters iters]
-      (let [runtime (time-body (rest (run-genetic-programming regression-options)))]
-        (with-open [wrtr (writer (str "reports/regression.fungp." now ".time") :append true)]
-         (.write wrtr (str (first runtime) "," (last (last runtime)) "\n")))
-      (if (> iters 0)
-        (recur (- iters 1))
-        runtime))))
+  (let [options (conj {:report (test-report report-name)} options)]
+	  (loop [iters samples]
+	    (let [runtime (time-body (rest (run-fungp-moses options)))]
+	      (with-open [wrtr (writer (str "reports/" report-name now ".time") :append true)]
+	        (.write wrtr (str (first runtime) "," (last (last runtime)) "\n")))
+	      (if (> iters 0)
+	        (recur (- iters 1))
+	        runtime)))))
--- src/fungp/test/util.clj
+++ src/fungp/test/util.clj
@@ -1,5 +1,21 @@
-(ns fungp.test.util)
+(ns fungp.test.util
+  (:use clojure.pprint)
+  (:use clojure.java.io))
 
 (defn ts [] (.format (new java.text.SimpleDateFormat "YYYYMMdd.HHmmss") (new java.util.Date)))
 
 (def now (ts))
+
+(defn test-report
+  "Reporting function. Prints out the tree and its score, to stdout and the report specified"
+  [report-name]
+  (with-open [wrtr (writer (str "reports/" report-name "." now ".report") :append true)]
+    (.write wrtr (str "Iteration,Best-tree\n")))
+  (fn 
+    [iteration best-tree best-fit]
+      (println iteration best-tree best-fit)
+      (with-open [wrtr (writer (str "reports/" report-name "." now ".report") :append true)]
+        (.write wrtr (str iteration "," best-fit "\n")))
+	    (pprint best-tree)
+	    (println (str "Iteration: " iteration "\tError:\t" best-fit "\n"))
+	    (flush)))
