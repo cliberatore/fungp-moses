089fc8a16bd1bade6e4b1ddee9416047844b3551
cliberatore <chris.liberatore@gmail.com>
2012-12-04 14:29:06 -0800

    Finished San Mateo testing framework. Sanmateo now tests generalization

--- src/fungp/test/problems/sanmateo.clj
+++ src/fungp/test/problems/sanmateo.clj
@@ -45,3 +45,14 @@
                   :functions ant-functions
                   :num-islands 1 :population-size 10
                   :adf-count 2})
+
+(defn test-sanmateo [report-name samples options]
+  (let [options (conj {:report (test-report report-name)} options)]
+	  (loop [iters samples]
+	    (let [runtime (time-body (rest (run-fungp-moses options)))]
+	      (with-open [wrtr (writer (str "reports/" report-name "." now ".time") :append true)]
+	        (.write wrtr (str (first runtime) "," (last (last runtime)) "\n")))
+	      (if (> iters 0)
+	        (recur (- iters 1))
+	        (with-open [wrtr (writer (str "reports/" report-name "." now ".generalization") :append true)]
+	          (.write wrtr (str (run-ant (nth (last runtime) 1) TESTFOOD) "\n"))))))))
